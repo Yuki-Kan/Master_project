cd0a2cb66c086ef09298063a8bda86c2b927a0c4
Yuki-Kan <z.kan@student.rug.nl>
2019-02-18 22:33:50 +0100

    Add class toss. No need to generate all datasets each time step

--- Matlab/Generate_data.m
+++ Matlab/Generate_data.m
@@ -1,12 +1,13 @@
-function [ rand_data, rand_lbl ] = Generate_data(cls_ctrs, p1, prots, if_plot)
+function [fvec, lbl] = Generate_data(cls_ctrs, prots)
 %ARTI_DATA Summary of this function goes here
 %   Detailed explanation goes here
 N = 2; % number of classes
-Num_all = 500; % number of all the points
+Num_all = 400; % number of all the points
 
-p2 = 1 - p1;
-Num1 = round(p1 * Num_all);
-Num2 = round(p2 * Num_all);
+% p2 = 1 - p1;
+p = 0.5; 
+Num1 = round(p * Num_all);
+Num2 = round(p * Num_all);
 clt = [Num1 Num2]; % store the number of points each cluster
 
 var = 0.4; % sigma for the normrnd spread
@@ -30,34 +31,35 @@ for i = 1:N
 end
 
 % decide if plot figures or not
-if if_plot == 1
-   figure;
-   hold on;
-   sz = 20;
-   
-   for i = 1:N
-       S = clt(i); % number of points each class
-       if i == 1
-           rows = 1:S;
-       else
-           rows = clt(i-1)+1:clt(i-1)+S;
-       end
-       scatter(fvec(rows,1), fvec(rows,2),sz, 'filled');
+
+figure;
+hold on;
+sz = 20;
+
+for i = 1:N
+   S = clt(i); % number of points each class
+   if i == 1
+       rows = 1:S;
+   else
+       rows = clt(i-1)+1:clt(i-1)+S;
    end
-
-   % plot prototypes
-   gscatter([prots(1,1); prots(2,1)],[prots(1,2); prots(2,2)],[prots(1,3); prots(2,3)], 'mg', '..',25);
-   xlim([-3 5])
-   ylim([-3 5])
-   hold off;
-   legend(['p1=' num2str(p1)],['p2=' num2str(p2)])
-
+   scatter(fvec(rows,1), fvec(rows,2),sz, 'filled');
 end
 
-%randomly select one datapoint at timestep t
-rand_ind = randi(Num_all);
-rand_data = fvec(rand_ind, :);
-rand_lbl = lbl(rand_ind);
+% plot prototypes
+gscatter([prots(1,1); prots(2,1)],[prots(1,2); prots(2,2)],[prots(1,3); prots(2,3)], 'mg', '..',25);
+xlim([-3 5])
+ylim([-3 5])
+hold off;
+%    legend(['p1=' num2str(p1)],['p2=' num2str(p2)])
+legend('class1', 'class2')
+
+
+
+% %randomly select one datapoint at timestep t
+% rand_ind = randi(Num_all);
+% rand_data = fvec(rand_ind, :);
+% rand_lbl = lbl(rand_ind);
 
 end
 
--- Matlab/Virtual_drift.m
+++ Matlab/Virtual_drift.m
@@ -2,11 +2,16 @@
 a0 = 50;
 a_end = 200;
 p_max = 0.8;
-L = 2;              % cluster drift distance
+L = 1;              % cluster drift distance
 eta = 0.05;         % learning rate
-N = 2;              % data dimention 
+N = 100;              % data dimention 
 conti_ls = 1/N;     % continue learning step
 tic;
+
+% generate artificial dataset
+[fvec, lbl] = Generate_data(cls_ctrs, prots);
+lbl1_idx = find(lbl==1);
+lbl2_idx = find(lbl==2);
 
 % cluster centres (0,0) and (-L,L)
 c1 = [0 ,L];
@@ -20,7 +25,7 @@ prots = [prots_feature prots_lbl];
 
 % Linear
 p_init = 0.5;
-[cls_w1, cls_w2] = Linear(a0, a_end, p_init, p_max);
+[cls_p1, cls_p2] = Linear(a0, a_end, p_init, p_max);
 
 % % Sudden change
 % p_max_sudden = 0.75;
@@ -43,14 +48,20 @@ ref_error_array = [];
 err1_array = [];
 err2_array = [];
 
-if_plot = 1;
 % conti_ls = 20;
 for t = 1:a_end
-    current_w1 = cls_w1(t);
-    [selected_data, data_lable] = Generate_data(cls_ctrs, current_w1, prots, if_plot);
+    current_p1 = cls_p1(t);
+    data_label = Class_toss(current_p1);
+    
+%   randomly select one example with respect to the label
+    if data_label == 1
+        selected_data = fvec(randsample(lbl1_idx,1),:);
+    elseif data_label == 2
+        selected_data = fvec(randsample(lbl2_idx,1),:);
+    end
    
     % use the randomly selected datapoint to update prots
-    prots = LVQ1_algorithm(selected_data, data_lable, prots, eta);
+    prots = LVQ1_algorithm(selected_data, data_label, prots, eta);
     proto1 = prots(1, 1:2);
     proto2 = prots(2, 1:2);
     protos_array = [protos_array; prots];
@@ -72,7 +83,7 @@ gerr2_array = double(err2_array);
 % plot tracking error and reference error
 figure;
 hold on
-plot(1:a_end, tra_err_array,'-x','MarkerSize',4);
+plot(1:a_end, tra_err_array,'-*','MarkerSize',4);
 plot(1:a_end, ref_err_array,'-x','MarkerSize',4);
 plot(1:a_end, gerr1_array)
 plot(1:a_end, gerr2_array)
