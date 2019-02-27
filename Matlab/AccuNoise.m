3169527d996e19b415cbb809bb675627153d38d0
Yuki-Kan <z.kan@student.rug.nl>
2019-02-27 16:52:11 +0100

    add randomized noise and randomly generate new example

--- Matlab/AccuNoise.m
+++ Matlab/AccuNoise.m
@@ -2,28 +2,79 @@ function [proto1_new, proto2_new] = AccuNoise( Q11, Q12, Q22, eta, N, proto1, pr
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
-syms a b 
-eqns1 = [a*Q11+b*Q12 == 1-eta/N, a^2*Q11 + b^2*Q22 + 2*a*b*Q12==Q11];
-vars1 = [a b];
-S1 = solve(eqns1, vars1);
+% random vector
+r = rand(1, N);
+r = r/norm(r);
+
+syms a b c d e f
+eqn1 = a*Q11 + b*Q12 + c*r*proto1' == 1-eta/N;  % w1_*w1 = 1-eta/N
+eqn2 = a^2*Q11 + b^2*Q22 + c^2*r*r' + 2*a*b*Q12 + 2*a*c*r*proto1' + 2*a*c*r*proto2' == Q11; 
+eqn3 = d*Q12 + e*Q22 + f*r*proto2'== 1-eta/N;
+eqn4 = d^2*Q11 + e^2*Q22 + f^2*r*r' + 2*d*e*Q12 + 2*d*f*r*proto1' + 2*e*f*r*proto2' == Q22; 
+eqn5 = a*d*Q11 + a*e*Q12 + (a*f+c*d)*r*proto1' + b*d*Q12 + b*e*Q22 + (b*f+c*e)*r*proto2'+ c*f*r*r' == Q12; 
+eqns = [eqn1, eqn2, eqn3, eqn4, eqn5];
+vars = [a b c d e f];
+S1 = solve(eqns, vars);
 sola = double(S1.a);
 solb = double(S1.b);
+solc = double(S1.c);
+sold = double(S1.d);
+sole = double(S1.e);
+solf = double(S1.f);
 
-random = randsample(2,1,true,[1 2]);
-proto1_new = sola(random)*proto1 + solb(random)*proto2;
 
 
-syms c d
-eqns2 = [c*Q12+d*Q22 == 1-eta/N, c^2*Q11 + d^2*Q22 + 2*c*d*Q12==Q22];
-vars2 = [c d];
-S2 = solve(eqns2, vars2);
-solc = double(S2.c);
-sold = double(S2.d);
 
-proto2_new = solc(random)*proto1 + sold(random)*proto2;
+% 
+% syms a b 
+% eqn1 = a*Q11+b*Q12 == 1-eta/N;
+% eqn2 = a^2*Q11 + b^2*Q22 + 2*a*b*Q12==Q11;
+% eqns1 = [eqn1, eqn2];
+% vars1 = [a b];
+% S1 = solve(eqns1, vars1);
+% sola = double(S1.a);
+% solb = double(S1.b);
+% 
+% random = randsample(2,1,true,[1 2]);
+% proto1_new = sola(random)*proto1 + solb(random)*proto2;
+% 
+% 
+% syms c d
+% eqns2 = [c*Q12+d*Q22 == 1-eta/N, c^2*Q11 + d^2*Q22 + 2*c*d*Q12==Q22];
+% vars2 = [c d];
+% S2 = solve(eqns2, vars2);
+% solc = double(S2.c);
+% sold = double(S2.d);
+
+% syms a b c d 
+% eqns1 = [a*Q11+b*Q12 == 1-eta/N, ...
+%          a^2*Q11 + b^2*Q22 + 2*a*b*Q12==Q11, ...
+%          c*Q12+d*Q22 == 1-eta/N, ...
+%          c^2*Q11 + d^2*Q22 + 2*c*d*Q12==Q22, ...
+%          a*c*Q11 + a*d*Q12 + b*c*Q12 + b*d*Q22 == Q12];
+% vars1 = [a b c d];
+% S1 = solve(eqns1, vars1);
+% sola = double(S1.a);
+% solb = double(S1.b);
+% solc = double(S2.c);
+% sold = double(S2.d);
+
+random = randsample(4,1);
+a = real(sola(random, :));
+b = real(solb(random, :));
+c = real(solc(random, :));
+d = real(sold(random, :));
+e = real(sole(random, :));
+f = real(solf(random, :));
+
+proto1_new = a*proto1 + b*proto2 + c*r;
+proto2_new = d*proto1 + e*proto2 + f*r;
 
 % to test if q11 == Q11, q22 == Q22
 q11 = proto1_new * proto1_new';
 q22 = proto2_new * proto2_new';
+
+q12 = proto1_new * proto2_new';
+q21 = proto2_new * proto1_new';
 
 end
--- Matlab/Virtual_drift.m
+++ Matlab/Virtual_drift.m
@@ -18,6 +18,10 @@ c2 = zeros(1, N);
 c2(1) = 1;
 cls_ctrs = [c1; c2];
 
+% c1*c1'
+% c2*c2'
+% c1*c2'
+
 % Initial prototypes
 prots_feature = [c1; c2];
 prots_lbl = [1;2];
@@ -56,20 +60,27 @@ ref_error_array = [];
 err1_array = [];
 err2_array = [];
 
+% parameters of Gaussian distribution
+var = 0.4; % sigma for the normrnd spread
+sigma = sqrt(var);
+
 i = 1;
 for t = 1:ls:a_end 
     current_p1 = cls_p1(i);
     data_label = Class_toss(current_p1);
+    
+    % randomly generate new example based on the label
+    new_data = normrnd(cls_ctrs(data_label, :), sigma, [1, N]); 
 
-%   randomly select one example with respect to the label
-    if data_label == 1
-        selected_data = fvec(randsample(lbl1_idx,1),:);
-    elseif data_label == 2
-        selected_data = fvec(randsample(lbl2_idx,1),:);
-    end
+% %   randomly select one example with respect to the label
+%     if data_label == 1
+%         new_data = fvec(randsample(lbl1_idx,1),:);
+%     elseif data_label == 2
+%         new_data = fvec(randsample(lbl2_idx,1),:);
+%     end
 
     % use the randomly selected datapoint to update prots
-    prots = LVQ1_algorithm(selected_data, data_label, prots, eta, N);
+    prots = LVQ1_algorithm(new_data, data_label, prots, eta, N);
     proto1 = prots(1, 1:N);
     proto2 = prots(2, 1:N);
     
@@ -94,9 +105,9 @@ for t = 1:ls:a_end
 %     ref_error_array = [ref_error_array; ref_err];
 %     tra_error_array = [tra_error_array; tra_err];
 
-    % add noise
-    [proto1_new, proto2_new] = AccuNoise(Q11, Q12, Q22, eta, N, proto1, proto2);
-    prots = [[proto1_new ; proto2_new] prots_lbl];
+%     % add noise
+%     [proto1_new, proto2_new] = AccuNoise(Q11, Q12, Q22, eta, N, proto1, proto2);
+%     prots = [[proto1_new ; proto2_new] prots_lbl];
     
     i = i+1;
 end
