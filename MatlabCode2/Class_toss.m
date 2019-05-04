5768c48751e5a61b89eb53963824f2bf14903915
Yuki-Kan <z.kan@student.rug.nl>
2019-05-04 15:50:10 +0200

    cleaning

--- MatlabCode2/Class_toss.m
+++
@@ -1,9 +0,0 @@
-function [ random_label ] = Class_toss(p1)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-
-p2 = 1-p1;
-random_label = randsample(2,1,true,[p1 p2]);
-
-end
-
